4099a7d7376f0585c8b0c7deb3bb0431bc20fa7c
Hugo González Labrador <hglavra@gmail.com>
2015-03-23 23:28:30 +0100

    Added container app

---
+++ assets/js/apps/container/container.js
@@ -0,0 +1,21 @@
+"use strict"
+
+Sanato.module("ContainerApp", function(ContainerApp, Sanato, Backbone, Marionette, $, _) {
+	this.startWithParent = false;
+
+	ContainerApp.LayoutView = Marionette.LayoutView.extend({
+		template: "#container-app-template",
+		regions: {
+			header: "#header",
+			notification: "#notification",
+			app: "#app",
+			footer: "#footer",
+		}
+	});
+
+	ContainerApp.on("start", function() {
+		ContainerApp.layoutView = new ContainerApp.LayoutView();
+		Sanato.getRegion("main").show(ContainerApp.layoutView);
+		// start ResourcesApp here
+	});
+});
