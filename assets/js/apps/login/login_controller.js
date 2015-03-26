d1c7667687c516de888dbb31bf5b38b92d196ff3
Hugo González Labrador <hglavra@gmail.com>
2015-03-26 21:31:05 +0100

    Improved organization of LoginApp

--- assets/js/apps/login/login_controller.js
+++ assets/js/apps/login/login_controller.js
@@ -1,2 +1,17 @@
 "use strict"
 
+Sanato.module("LoginApp", function(LoginApp, Sanato, Backbone, Marionette, $, _) {
+	LoginApp.Controller = {
+		login: function(username, password) {
+			var loging = Sanato.request("loginapp:login", username, password);
+			$.when(loging).done(function(data) {
+				localStorage.setItem("sanato_token", data);
+				Sanato.ContainerApp.start();
+				Sanato.ResourcesApp.start();
+			});
+			$.when(loging).fail(function(data, status) {
+				alert("Wrong credentials");
+			});
+		}
+	}
+});
--- assets/js/apps/login/login_model.js
+++ assets/js/apps/login/login_model.js
@@ -1,1 +1,28 @@
 "use strict"
+
+Sanato.module("LoginApp", function(LoginApp, Sanato, Backbone, Marionette, $, _) {
+	var API = {
+		login: function(username, password) {
+			var defer = $.Deferred();
+			$.ajax({
+				url: Sanato.config.baseURL + "auth/login",
+				type: 'POST',
+				async: true,
+				data: JSON.stringify({ "username": username, "password" : password }),
+				contentType: 'application/json; charset=utf-8',
+				dataType: 'json',
+				success: function(data) {
+					defer.resolve(data.token);
+				},
+				error: function() {
+					defer.reject();
+				}
+			});
+			return defer.promise();
+		},
+	}
+
+	Sanato.reqres.setHandler("loginapp:login", function(username, password) {
+		return API.login(username, password);
+	})
+});
